0c8e34cf728dca836dea27423fef854a26cf28f3
Johan Brodin <johan.brodin@gmail.com>
2014-04-26 13:41:17 +0200

    css

--- project/web-server/client/assets/js/scripts.js
+++ project/web-server/client/assets/js/scripts.js
@@ -1,1 +1,13 @@
-console.log("Test")
+var socket = io.connect('http://localhost');
+socket.on('tweet', function (data) {
+
+  console.debug("Got tweet", data.tweet);
+  var tweets = document.getElementById("tweets");
+  var item = document.createElement("li");
+  item.innerHTML = data.tweet.text;
+  item.setAttribute("class",data.tweet.emotions[0]);
+
+
+  tweets.insertBefore(item, tweets.firstChild);
+  
+});
